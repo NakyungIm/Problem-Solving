2f9f37d99e5e6dfd812e1bc613c7904e65a3de4a
NakyungIm <nakyoung99@gmail.com>
2024-12-16 10:56:21 -0500

    Valid_Palindrome

---
+++ Two_Pointers/Valid_Palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r"[^\uAC00-\uD7A30-9a-zA-Z\s]", "", s).lower()
+        s = s.replace(" ", "")
+        n = len(s)
+        for i in range(0, int(n/2)):
+            if s[i] != s[n-1-i]:
+                return False
+            else:
+                pass
+        return True
