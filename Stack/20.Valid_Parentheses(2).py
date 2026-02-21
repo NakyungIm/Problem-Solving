2e245f1e0a962f38567faf88e588d5125df79a1e
NakyungIm <nakyoung99@gmail.com>
2026-02-21 16:19:42 -0500

    20

--- Stack/20.Valid_Parentheses(2).py
+++ Stack/20.Valid_Parentheses(2).py
@@ -3,13 +3,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        mapping = {')':'(', '}':'{', ']':'['}
-
-        for i in s:
-            if i in mapping: # close bracket
-                if len(stack) == 0 or stack[-1] != mapping[i]:
-                    return False
-                stack.pop()
-            else: stack.append(i) # open bracket
-        return len(stack) == 0
+        brackets = {')':'(', '}':'{', ']':'['}
         
+        for i in s:
+            if i in brackets: # if i is in bracket keys
+                if stack == 0 or stack[-1] != brackets[i]: # if stack is empty or last element in stack is not equal to (, {, [
+                    return False
+                else: # brackets are matched
+                    stack.pop()
+            else: # if is is not in bracket keys
+                stack.append(i)
+        return len(stack) == 0
